57ff974e1eb090e8c8f83448c4df70fdbc3352e1
Fabian <merchanf@javeriana.edu.co>
2016-03-12 15:01:26 -0500

    pruebas antes de irme al estereo

--- client/js/taquin.js
+++ client/js/taquin.js
@@ -8,6 +8,9 @@ $( document ).ready(function() {
 
 /*
 * esta función maneja los eventos de clic de una celda de la tabla
+*
+* hasta este momento hay un eror y es sí los mueve, pero los vuelve a mover al estado inicial
+* ver la consola del navegador.
 */
 $(document).on('click', 'td', function(e) {
     var vacio;
@@ -18,33 +21,33 @@ $(document).on('click', 'td', function(e) {
                 /*
                 *   Tip: haciendo $('.'+matriz[i][j]) ya tiene el elemento que tiene que cambiar
                 */
-                var temp;
+                var temp='';
                 if( i-1>0 && matriz[i-1][j]=='*'){
                     //la posición vacía cambia con el de debajo
                     console.log('mover arriba');
                     temp = matriz[i-1][j];
-                    matriz[i-1][j] = matriz[i][j]
+                    matriz[i-1][j] = matriz[i][j];
                     matriz[i][j] = temp;
                     renderizarMatriz(matriz);
                 }else if (j-1>0 && matriz[i][j-1]=='*'){
                     //la posición vacía cambia con el de la derecha
                     console.log('mover izquierda');
                     temp = matriz[i][j-1];
-                    matriz[i][j-1] = matriz[i][j]
+                    matriz[i][j-1] = matriz[i][j];
                     matriz[i][j] = temp;
                     renderizarMatriz(matriz);
                 }else if (i+1<matriz.length && matriz[i+1][j]=='*'){
                     //la posición vacía cambia con el de encima
                     console.log('mover abajo');
                     temp = matriz[i+1][j];
-                    matriz[i+1][j] = matriz[i][j]
+                    matriz[i+1][j] = matriz[i][j];
                     matriz[i][j] = temp;
                     renderizarMatriz(matriz);
                 }else if (j+1<matriz.length && matriz[i][j+1]=='*'){
                     //la posición vacía cambia con el de la izquierda
                     console.log('mover derecha');
                     temp = matriz[i][j+1];
-                    matriz[i][j+1] = matriz[i][j]
+                    matriz[i][j+1] = matriz[i][j];
                     matriz[i][j] = temp;
                     renderizarMatriz(matriz);
                 }else{
